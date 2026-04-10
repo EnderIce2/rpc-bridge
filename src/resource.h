96a41ea390cb61d5f1c050cf54727bbc78448cc3
enderice2 <enderice2@protonmail.com>
2026-04-10 04:59:08 +0300

    refactor: change version string definition for clarity
    Signed-off-by: enderice2 <enderice2@protonmail.com>

--- src/resource.h
+++ src/resource.h
@@ -5,5 +5,9 @@
 #define IDM_HELP_ABOUT 40003
 #define IDM_VIEW_LOG 40004
 
+/* Current version */
 #define VER_VERSION 1, 4, 1, 2
-#define VER_VERSION_STR "1.4.1.2\0"
+
+#define VERSION_STR_EXPAND(x, y, z, w) #x "." #y "." #z "." #w "\0"
+#define VERSION_STR(x) VERSION_STR_EXPAND(x)
+#define VER_VERSION_STR VERSION_STR(VER_VERSION)
